2f72357fb0fd1b02349d5fe6362e7d7f34e14bbb
admin <esleynathan@hotmail.com>
2023-07-18 20:24:33 -0300

    Django Middlewares | 04 - Intercepetando responses

--- middlewares/middlewares.py
+++ middlewares/middlewares.py
@@ -7,10 +7,14 @@ def middleware_forbiden(get_response):
         # Aqui vai o código a ser executado antes da view
         # vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
 
-        if request.META['REMOTE_ADDR'] == '127.0.0.1':
-            return HttpResponse('Você está bloqueado.', status=403)
+        #if request.META['REMOTE_ADDR'] == '127.0.0.1':
+        #    return HttpResponse('Você está bloqueado.', status=403)
 
         response = get_response(request)
+
+        if request.META['REMOTE_ADDR'] == '127.0.0.1':
+            response.content = b'<h1>Acesso negado.</h1>'
+            response.status_code = 403
 
 
         # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
