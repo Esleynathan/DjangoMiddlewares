f50395f9be6935d20816affa2bdf1205bc99999e
admin <esleynathan@hotmail.com>
2023-07-18 20:26:34 -0300

    Django Middlewares | 05 - Maneira alternativa com classes

--- middlewares/middlewares.py
+++ middlewares/middlewares.py
@@ -23,3 +23,15 @@ def middleware_forbiden(get_response):
         return response
 
     return middleware
+
+
+#class MiddlewareForbiden:
+#    def __init__(self, get_response):
+#        self.get_response = get_response
+        
+
+#    def __call__(self, request):
+   
+#        response = self.get_response(request)
+
+#        return response
